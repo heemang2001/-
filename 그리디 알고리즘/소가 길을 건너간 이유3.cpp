44d249b7d23e5884731f5c90bf9b1be9b655c88f
heemang2001 <babypooh2001@naver.com>
2021-06-14 14:44:45 +0900

    소가 길을 건너간 이유3

--- 그리디 알고리즘/소가 길을 건너간 이유3.cpp
+++ 그리디 알고리즘/소가 길을 건너간 이유3.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
+int N;
+
 int main()
-{
-	//test
+{	
+	int answer = 0;
+	vector<pair<int, int>> vecCowtime;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int nTemp1, nTemp2;	// 도착시간, 검문시간
+
+		cin >> nTemp1 >> nTemp2;
+
+		vecCowtime.push_back({ nTemp1, nTemp2 });
+	}
+
+	sort(vecCowtime.begin(), vecCowtime.end());
+
+	answer = vecCowtime[0].first + vecCowtime[0].second;
+
+	for (int i = 1; i < N; i++)
+	{
+		if (answer < vecCowtime[i].first)
+		{
+			answer += vecCowtime[i].first - answer;
+		}
+
+		answer += vecCowtime[i].second;
+	}
+
+	cout << answer << '\n';
 
 	return 0;
 }
