ed3b8dcbb55d1ba8b5f8eda150ba6bdbb620f5d1
heemang2001 <babypooh2001@naver.com>
2021-07-09 16:41:17 +0900

    수리공 항승

--- 그리디 알고리즘/수리공 항승.cpp
+++ 그리디 알고리즘/수리공 항승.cpp
@@ -1,68 +1,103 @@
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//
+//using namespace std;
+//
+//int N;
+//int L; // 0 <= N,L <= 1000 
+//
+//int main()
+//{
+//	cin >> N >> L;
+//
+//	vector<pair<int, int>> vecWater;	
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		int nTemp;
+//		cin >> nTemp;
+//		vecWater.push_back({nTemp,0});
+//	}
+//
+//	sort(vecWater.begin(), vecWater.end());
+//
+//	int Lenth = L;
+//	bool bConnect = false;
+//
+//	if (N > 1)
+//	{
+//		vecWater[0].second = 1;
+//		for (int i = 0; i < vecWater.size() - 1; i++)
+//		{
+//			int nTemp = vecWater[i + 1].first + 0.5 - (vecWater[i].first - 0.5);
+//			int nTemp2 = vecWater[i + 1].first + 0.5 - (vecWater[i].first + 0.5);
+//
+//			if (Lenth >= nTemp2 && bConnect)
+//			{
+//				Lenth -= nTemp2;
+//				vecWater[i + 1].second = vecWater[i].second;
+//			}
+//
+//			// 이을수 있으면
+//			else if (Lenth >= nTemp && !bConnect)
+//			{
+//				Lenth -= nTemp;
+//				vecWater[i + 1].second = vecWater[i].second;
+//				bConnect = true;
+//			}
+//
+//			// 이을수 없으면
+//			else
+//			{
+//				Lenth = L;
+//				vecWater[i + 1].second = vecWater[i].second + 1;
+//				bConnect = false;
+//			}
+//		}
+//
+//		cout << vecWater[N - 1].second << '\n';
+//	}
+//
+//	else
+//	{
+//		cout << 1 << '\n';		
+//	}
+//
+//	return 0;
+//}
+
 #include<iostream>
 #include<algorithm>
-#include<vector>
 
 using namespace std;
 
-int N;
-int L; // 0 <= N,L <= 1000 
-
+int list[1001];
 int main()
 {
+	int ans = 1;
+	int N, L;
+
 	cin >> N >> L;
 
-	vector<pair<int, int>> vecWater;	
-
 	for (int i = 0; i < N; i++)
+		cin >> list[i];
+
+
+	sort(list, list + N);
+
+	int st = list[0];
+
+	for (int i = 1; i < N; i++)
 	{
-		int nTemp;
-		cin >> nTemp;
-		vecWater.push_back({nTemp,0});
-	}
-
-	sort(vecWater.begin(), vecWater.end());
-
-	int Lenth = L;
-	bool bConnect = false;
-
-	if (N > 1)
-	{
-		vecWater[0].second = 1;
-		for (int i = 0; i < vecWater.size() - 1; i++)
+		if (list[i] - st + 1 > L)
 		{
-			int nTemp = vecWater[i + 1].first + 0.5 - (vecWater[i].first - 0.5);
-			int nTemp2 = vecWater[i + 1].first + 0.5 - (vecWater[i].first + 0.5);
-
-			if (Lenth >= nTemp2 && bConnect)
-			{
-				Lenth -= nTemp2;
-				vecWater[i + 1].second = vecWater[i].second;
-			}
-
-			// 이을수 있으면
-			else if (Lenth >= nTemp && !bConnect)
-			{
-				Lenth -= nTemp;
-				vecWater[i + 1].second = vecWater[i].second;
-				bConnect = true;
-			}
-
-			// 이을수 없으면
-			else
-			{
-				Lenth = L;
-				vecWater[i + 1].second = vecWater[i].second + 1;
-				bConnect = false;
-			}
+			ans++;
+			st = list[i];
 		}
 
-		cout << vecWater[N - 1].second << '\n';
 	}
 
-	else
-	{
-		cout << 1 << '\n';		
-	}
-
+	cout << ans;
 	return 0;
 }
